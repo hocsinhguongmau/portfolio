e76f15bbac4de8354549aed9f13427125a954de9
thang241190 <thangnguyen24111990@gmail.com>
2022-05-21 00:34:11 +0300

    Add home bg

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,44 +1,42 @@
 import { motion } from 'framer-motion'
-import Lottie from 'lottie-react'
 import styled from 'styled-components'
-import frontend from '~/lotties/frontend.json'
-
-const WrapAnimation = styled.div`
-  width: 66.66%;
-  margin-left: auto;
-  margin-right: auto;
-`
-
-const Animation = () => {
-  return (
-    <WrapAnimation>
-      <Lottie animationData={frontend} autoplay={true} loop={true} />
-    </WrapAnimation>
-  )
-}
+import useToggleStore from '~/store/themeStore'
 
 const Wrapper = styled.div`
   margin-left: auto;
   margin-right: auto;
   position: relative;
-  background: #000;
-  border-radius: 9999px;
   text-align: center;
   max-width: 64rem;
 `
 
 const Title = styled.p`
-  font-size: 1.875rem;
-  line-height: 2.25rem;
+  font-size: 2.5rem;
+  line-height: 3rem;
   font-weight: bold;
-  margin-bottom: 0.5rem;
+  margin-bottom: 0;
 `
 
 const Line = styled.p`
-  font-size: 1.5rem /* 24px */;
-  line-height: 2rem /* 32px */;
+  font-size: 1.2rem /* 24px */;
+  line-height: 1rem /* 32px */;
 `
-
+const WrapBgMain = styled.div`
+  width: 50%;
+  margin: auto;
+  img {
+    width: 100%;
+  }
+`
+const TextWrapper = styled.div`
+  position: absolute;
+  top: 50%;
+  width: calc(50% - 4rem);
+  left: 50%;
+  margin-top: -1rem;
+  transform: translate(-50%, -50%);
+  z-index: 10;
+`
 const Home = () => {
   const variants = {
     initial: { opacity: 0, y: '100px', scale: 0.5 },
@@ -49,12 +47,22 @@ const Home = () => {
       transition: { duration: 0.3 },
     },
   }
+  const toggle = useToggleStore((state) => state.toggle)
   return (
     <motion.div variants={variants} initial='initial' animate='animate'>
       <Wrapper>
-        <Title>Hello</Title>
-        <Line>My name is Thang Nguyen</Line>
-        <Line>Frontend developer based in Helsinki</Line>
+        <WrapBgMain>
+          {toggle ? (
+            <img src='/images/home-bg-dark.svg' />
+          ) : (
+            <img src='/images/home-bg-light.svg' />
+          )}
+        </WrapBgMain>
+        <TextWrapper>
+          <Title>Hello</Title>
+          <Line>My name is Thang Nguyen</Line>
+          <Line>Frontend developer based in Helsinki</Line>
+        </TextWrapper>
       </Wrapper>
     </motion.div>
   )
