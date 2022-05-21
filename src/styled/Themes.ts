e3be03544e37a89ec05e474d77194f1a79c40558
thang241190 <thangnguyen24111990@gmail.com>
2022-05-21 13:44:37 +0300

    Add home bg

--- src/styled/Themes.ts
+++ src/styled/Themes.ts
@@ -1,9 +1,16 @@
 export const BODY_LIGHT = '#FFF'
 export const TEXT_LIGHT = '#333'
 export const BACKGROUND_LIGHT = '#fceeb5'
+export const SVG_LIGHT_1 = '#99d3d3'
+export const SVG_LIGHT_2 = '#ffdc97'
+export const SVG_LIGHT_3 = '#ff9292'
+
 export const BODY_DARK = '#3E4758'
 export const TEXT_DARK = '#FFF'
 export const BACKGROUND_DARK = '#262E40'
+export const SVG_DARK_1 = '#676767'
+export const SVG_DARK_2 = '#262E40'
+export const SVG_DARK_3 = '#8b8b8b'
 
 export const lightTheme: ThemeType = {
   body: BODY_LIGHT,
