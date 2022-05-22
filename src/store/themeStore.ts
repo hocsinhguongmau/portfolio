1c476c0fc0e9e76a0b1d197d4568ec5319939ba0
thang241190 <thangnguyen24111990@gmail.com>
2022-05-22 17:21:55 +0300

    Save darkmode in localstorage

--- src/store/themeStore.ts
+++ src/store/themeStore.ts
@@ -1,8 +1,15 @@
 import create from 'zustand'
+import { persist } from 'zustand/middleware'
 
-const useToggleStore = create<ToggleState>((set) => ({
-  toggle: false,
-  setToggle: (toggle: boolean) => set(() => ({ toggle: toggle })),
-}))
-
+const useToggleStore = create(
+  persist(
+    (set: any) => ({
+      toggle: false,
+      setToggle: (toggle: boolean) => set(() => ({ toggle: toggle })),
+    }),
+    {
+      name: 'theme-storage',
+    },
+  ),
+)
 export default useToggleStore
